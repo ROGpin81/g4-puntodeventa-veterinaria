ed4d41bd715c631d9c069baf89ac9eaa4a9033f4
EAPerez007 <157754604+codisi97@users.noreply.github.com>
2025-12-04 21:00:45 -0600

    Conexion app.post

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const pool = mysql.createPool({
     host:'localhost',
     user:'root',
-    password:'',
+    password:'alienware1070',
     database:'VeterinariaBaseDatosPF'
 
 })
@@ -26,7 +26,7 @@ pool.getConnection((error,conexion)=>{
 
 
 //Prueba de servidor 
-app.get('/veterinaria',(req, res)=>{
+app.get('/usuarios',(req, res)=>{
     const sql = 'SELECT idusuario,username,email,password FROM usuarios';
 
     pool.query(sql,(err,results)=>{
@@ -41,6 +41,29 @@ app.get('/veterinaria',(req, res)=>{
     })
 });
  
+app.post('/usuarios',(req,res)=>{
+    const usuarios = req.body;
+    if(!usuarios.idusuario || !usuarios.username || !usuarios.email || !usuarios.password ){
+        res.status(200).json({status:400,message:'Los parametros datos son requeridos '});
+    }
+    else{
+        const sql = 'INSERT INTO usuarios (username , email, password) VALUES(?,?,?);';
+        pool.query(sql,[usuarios.username,usuarios.email,usuarios.password],(err,results)=>{
+            if(err){
+            console.log('Error en la consulta sql');
+            res.status(500).json({status:500,message:'Error en la consulta sql......'});
+
+        }else{
+            res.status(200).json({status:200,message:'Success',data:usuarios});
+        }
+
+
+
+
+        });
+    }
+
+});
 
 app.get('/llamada',(req,res)=>{
     return res.send('Llamada al 911');
